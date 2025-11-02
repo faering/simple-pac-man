e6725a78a9e81d9e66e6cb628eacde60a4fab86c
faering <mattiasha23@gmail.com>
2025-11-02 01:01:40 +0000

    feat(main): use sfml for graphics

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+// #include <optional>
 
 int main()
 {
-    std::cout << "Hello, Simple Pac-Man!" << std::endl;
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+
+    // Set the frame rate limit
+    // window.setFramerateLimit(60);
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear screen
+        // window.clear(sf::Color::Black);
+        window.clear();
+
+        // Update the window
+        window.display();
+    }
+
     return 0;
 }
